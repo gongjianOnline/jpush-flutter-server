92cbe7dea3e81ee8054b4cabdaca8d879a265cd7
李承斌 <gongjian@163.com>
2024-05-14 14:14:34 +0800

    极光推送服务端

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { JPushAsync } from "jpush-async";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    console.log(JPushAsync)
+    var client = JPushAsync.buildClient('appkey', '密钥')
+    client.push().setPlatform('ios', 'android')
+    .setAudience(JPushAsync.ALL)
+    .setNotification('Hi, JPush', JPushAsync.ios('ios alert', 'happy', 5))
+    .send()
+    .then(function(result) {
+        console.log(result)
+    }).catch(function(err) {
+        console.log(err)
+    })
+
+    return {
+      code:101,
+      message:'hello world',
+      data:null
+    };
   }
 }
+
+// const jpushFun = (client)=>{
+//   return new Promise((resolve)=>{
+    
+    
+//   })
+// }
